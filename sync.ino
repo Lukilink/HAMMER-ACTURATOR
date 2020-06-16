21ac99e63e4812508e197fc36688a1c5e80f538a
SelfdriveVanagon <41362285+Lukilink@users.noreply.github.com>
2020-06-16 22:50:21 +0200

    Update sync.ino

--- sync.ino
+++ sync.ino
@@ -2,7 +2,7 @@
 /* When you run this sketch, you will see 3 diffrent column in serial monitor.
  * the values in the second column are always a few milliseconds behind the values in first column.
  * The goal is, to add a delay to value and store it in "value_with_added_delay". 
- * So that the value_with_added_delay is synced to value_with_delay.
+ * So that the value_with_added_delay is showing the same like value_with_delay.
  */
 
 
